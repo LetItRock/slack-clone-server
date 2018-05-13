bdc3c413028b40872583b78dab2d9a45b80f8154
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-05-13 12:02:15 -0500

    - cors; - register user mutation; - password encryption; - user fields validation; - error schema;

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,44 @@
+import bcrypt from 'bcrypt';
+import _ from 'lodash';
+
+const formatErrors = (e, models) => {
+  if (e instanceof models.sequelize.ValidationError) {
+    return e.errors.map(x => _.pick(x, ['path', 'message']));
+  }
+  return [{ path: 'name', message: 'something went wrong' }];
+};
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...args }, { models }) => {
+      try {
+        if (models.User.isPasswordValid(password)) {
+          return {
+            ok: false,
+            errors: [
+              {
+                path: 'password',
+                message: 'The password needs to be between 8 and 18 characters long',
+              },
+            ],
+          };
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = await models.User.create({ password: hashedPassword, ...args });
+        return {
+          ok: true,
+          user,
+        };
+      } catch (e) {
+        return {
+          ok: false,
+          errors: formatErrors(e, models),
+        };
+      }
+    },
   },
 };
