7cdca9cbef8ad4782d0c523c36bd45f47c948dc4
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-05-17 20:04:47 -0500

    - message resolver requires user to be authenticated; - with a message fetch also user; - implement messages query;

--- resolvers/message.js
+++ resolvers/message.js
@@ -1,16 +1,21 @@
+import { requiresAuth } from '../permissions';
+
 export default {
+  Message: {
+    user: ({ userId }, args, { models }) => models.User.findOne({ where: { id: userId } }, { raw: true }),
+  },
   Query: {
-    messages: async (parent, args, { models, user }) => [],
+    messages: requiresAuth.createResolver(async (parent, { channelId }, { models, user }) =>
+      models.Message.findAll({ order: [['created_at', 'ASC']], where: { channelId } }, { raw: true })),
   },
   Mutation: {
-    createMessage: async (parent, args, { models, user }) => {
+    createMessage: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
         await models.Message.create({ ...args, userId: user.id });
         return true;
       } catch (e) {
-        console.log(e);
         return false;
       }
-    },
+    }),
   },
 };
