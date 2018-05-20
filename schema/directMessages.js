30bd0adefa01293e21cf90c59e74824ea7f46b2c
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-05-19 22:15:08 -0500

    - direct message resolver; - direct messages query and create mutation;

--- schema/directMessages.js
+++ schema/directMessages.js
@@ -2,15 +2,16 @@ export default `
   type DirectMessage {
     id: Int!
     text: String
+    created_at: String! 
     sender: User!
     receiverId: Int!
   }
 
   type Query {
-    directMessages: [DirectMessage!]!
+    directMessages(teamId: Int!, otherUserId: Int!): [DirectMessage!]!
   }
 
   type Mutation {
-    createDirectMessage(receiverId: Int!, text: String!): Boolean!
+    createDirectMessage(receiverId: Int!, text: String!, teamId: Int!): Boolean!
   }
 `;
