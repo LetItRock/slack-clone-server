0968114a87be9535a7d1933f656e03c0105c723e
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2018-06-24 13:00:07 +0200

    - graphql-redis-subscriptions;

--- pubSub.js
+++ pubSub.js
@@ -1,3 +1,14 @@
-import { PubSub } from 'graphql-subscriptions';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
-export default new PubSub();
+const REDIS_DOMAIN_NAME = 'localhost';
+const PORT_NUMBER = 6379;
+
+const pubsub = new RedisPubSub({
+  connection: {
+    host: REDIS_DOMAIN_NAME,
+    port: PORT_NUMBER,
+    retry_strategy: options => Math.max(options.attempt * 100, 3000), // reconnect after
+  },
+});
+
+export default pubsub;
